6037c9b65d9538a9cb3004d004c81eb93c157041
Broly <vidovicp20@gmail.com>
2022-10-29 19:44:12 -0400

    Manager and employee tests created and successful

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -1,1 +1,19 @@
 // Employee test
+
+// name: '';
+// id: '';
+// email:'';
+var Employee = require('../lib/Employee');
+
+
+test ('should generate name property where the value of a new employees name is kept', () => {
+    const newObj = new Employee('petar');
+    expect(newObj.name).toBe('petar');
+
+});
+
+test ('should generate name property where the value of a new employees name is kept', () => {
+    const newObj = new Employee('', 3);
+    expect(newObj.id).toBe(3);
+
+});
--- tests/manager.test.js
+++ tests/manager.test.js
@@ -1,1 +1,9 @@
 // manager test
+
+const Manager = require("../lib/Manager");
+
+test ('office number property for manager should be generated', () => {
+    const newObj = new Manager('', '', '', 3);
+    expect(newObj.officenumber).toBe(3);
+
+});
